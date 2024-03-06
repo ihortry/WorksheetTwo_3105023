8cfe9e64e13fa07b8328a47ad920997e5ff3617e
Ihor Tryndey <igortryndey@gmail.com>
2024-03-06 00:22:26 +0000

    stubs methods implementations inside ConversionsTest

--- src/main/java/griffith/Conversions.java
+++ src/main/java/griffith/Conversions.java
@@ -1,5 +1,41 @@
 package griffith;
 
-public class Conversions {
+public class Conversions implements ConversionsInterface{
+
+	@Override
+	public double celsiusToFahrenheit(double celsius) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double fahrenheitToCelsius(double fahrenheit) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double milesToKilometer(double miles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double kilometerToMiles(double kilometer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String upperCaseToLowerCase(String uStr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String lowerCaseToUpperCase(String lStr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ src/main/java/griffith/ConversionsInterface.java
@@ -0,0 +1,10 @@
+package griffith;
+
+public interface ConversionsInterface {
+	public double celsiusToFahrenheit(double celsius);
+	public double fahrenheitToCelsius(double fahrenheit);
+	public double milesToKilometer(double miles);
+	public double kilometerToMiles(double kilometer);
+	public String upperCaseToLowerCase(String uStr);
+	public String lowerCaseToUpperCase(String lStr);
+}
