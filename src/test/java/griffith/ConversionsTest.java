27f46dab893dc293fc4ba4633671846883460017
Ihor Tryndey <igortryndey@gmail.com>
2024-03-06 01:01:50 +0000

    celsiusToFahrenheitTest the unit test implementation

--- src/test/java/griffith/ConversionsTest.java
+++ src/test/java/griffith/ConversionsTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConversionsTest {
-
+	Conversions obj = new Conversions();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void celsiusTofahrenheitTest() {
+		double alpha = 0.01;
+		assertEquals(32, obj.celsiusToFahrenheit(0), alpha);
+		assertEquals(212, obj.celsiusToFahrenheit(100), alpha);
+		assertEquals(-25.6, obj.celsiusToFahrenheit(-32), alpha);
 	}
 
 }
